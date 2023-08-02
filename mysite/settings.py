6b682a52ec3bd013dd88b7b9f94e8bb7673c32ca
Janam-Ke <janamouko@gmail.com>
2023-08-02 08:47:42 +0300

    more changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+
 ]
 
 MIDDLEWARE = [
